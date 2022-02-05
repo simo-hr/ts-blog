39af2535adc0a5d9a0524e0086ece336850507e9
github xxbbxx825 <xxbbxx825@gmail.com>
2022-02-05 16:43:41 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    semi: [2, 'never'],
+    'no-undef': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'always',
+        imports: 'always',
+        exports: 'always',
+        functions: 'never',
+      }
+    ],
+    'vue/max-attributes-per-line': 'off',
+    'no-unused-vars': 'off', // 使っていない変数を許可
+    'vue/html-self-closing': 'off', // 空タグを許可する
+  },
+}
